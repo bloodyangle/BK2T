bed144d7046b2b1fe7db33a4fea16420d2668264
DESKTOP-2CP0C7F\maomao <1765785706@qq.com>
2019-01-09 12:54:50 +0800

    no message

--- libs/account/auth_lib.py
+++ libs/account/auth_lib.py
@@ -1,9 +1,12 @@
 
 def isIn(source,target):
-    count = 0
-    for index in source:
-        if index in target:
-            count += 1
-            if count <= len(source):
-                return True
-            return False
+    if source is None or target is None:
+        return False
+    else:
+        count = 0
+        for index in source:
+            if index in target:
+                count += 1
+                if count <= len(source):
+                    return True
+                return False
