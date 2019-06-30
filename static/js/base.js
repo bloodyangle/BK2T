8415dc6d286969224529c12c6c1bb797038de55d
DESKTOP-H0B2FP4\fdd <minwork0926@163.com>
2019-06-30 18:56:06 +0800

    no message

--- static/js/base.js
+++ static/js/base.js
@@ -1,94 +1,84 @@
 //监听div大小变化
 (function($, h, c) {
-	var a = $([]),
-	e = $.resize = $.extend($.resize, {}),
-	i,
-	k = "setTimeout",
-	j = "resize",
-	d = j + "-special-event",
-	b = "delay",
-	f = "throttleWindow";
-	e[b] = 250;
-	e[f] = true;
-	$.event.special[j] = {
-		setup: function() {
-			if (!e[f] && this[k]) {
-				return false;
-			}
-			var l = $(this);
-			a = a.add(l);
-			$.data(this, d, {
-				w: l.width(),
-				h: l.height()
-			});
-			if (a.length === 1) {
-				g();
-			}
-		},
-		teardown: function() {
-			if (!e[f] && this[k]) {
-				return false;
-			}
-			var l = $(this);
-			a = a.not(l);
-			l.removeData(d);
-			if (!a.length) {
-				clearTimeout(i);
-			}
-		},
-		add: function(l) {
-			if (!e[f] && this[k]) {
-				return false;
-			}
-			var n;
-			function m(s, o, p) {
-				var q = $(this),
-				r = $.data(this, d);
-				r.w = o !== c ? o: q.width();
-				r.h = p !== c ? p: q.height();
-				n.apply(this, arguments);
-			}
-			if ($.isFunction(l)) {
-				n = l;
-				return m;
-			} else {
-				n = l.handler;
-				l.handler = m;
-			}
-		}
-	};
-	function g() {
-		i = h[k](function() {
-			a.each(function() {
-				var n = $(this),
-				m = n.width(),
-				l = n.height(),
-				o = $.data(this, d);
-				if (m !== o.w || l !== o.h) {
-					n.trigger(j, [o.w = m, o.h = l]);
-				}
-			});
-			g();
-		},
-		e[b]);
-	}
+   var a = $([]),
+   e = $.resize = $.extend($.resize, {}),
+   i,
+   k = "setTimeout",
+   j = "resize",
+   d = j + "-special-event",
+   b = "delay",
+   f = "throttleWindow";
+   e[b] = 250;
+   e[f] = true;
+   $.event.special[j] = {
+      setup: function() {
+         if (!e[f] && this[k]) {
+            return false;
+         }
+         var l = $(this);
+         a = a.add(l);
+         $.data(this, d, {
+            w: l.width(),
+            h: l.height()
+         });
+         if (a.length === 1) {
+            g();
+         }
+      },
+      teardown: function() {
+         if (!e[f] && this[k]) {
+            return false;
+         }
+         var l = $(this);
+         a = a.not(l);
+         l.removeData(d);
+         if (!a.length) {
+            clearTimeout(i);
+         }
+      },
+      add: function(l) {
+         if (!e[f] && this[k]) {
+            return false;
+         }
+         var n;
+         function m(s, o, p) {
+            var q = $(this),
+            r = $.data(this, d);
+            r.w = o !== c ? o: q.width();
+            r.h = p !== c ? p: q.height();
+            n.apply(this, arguments);
+         }
+         if ($.isFunction(l)) {
+            n = l;
+            return m;
+         } else {
+            n = l.handler;
+            l.handler = m;
+         }
+      }
+   };
+   function g() {
+      i = h[k](function() {
+         a.each(function() {
+            var n = $(this),
+            m = n.width(),
+            l = n.height(),
+            o = $.data(this, d);
+            if (m !== o.w || l !== o.h) {
+               n.trigger(j, [o.w = m, o.h = l]);
+            }
+         });
+         g();
+      },
+      e[b]);
+   }
 })(jQuery, this);
 $(function(){
-	//时间选择控件
-	$.fn.datetimepicker.dates['zh'] = {
-		days:       ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六","星期日"],
-		daysShort:  ["日", "一", "二", "三", "四", "五", "六","日"],
-		daysMin:    ["日", "一", "二", "三", "四", "五", "六","日"],
-		months:     ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月","十二月"],
-		monthsShort:  ["一", "二", "三", "四", "五", "六", "七", "八", "九", "十", "十一", "十二"],
-		meridiem:    ["上午", "下午"],
-		//suffix:      ["st", "nd", "rd", "th"],
-		today:       "今天"
-	};
+
 })
 //封装id
 function createKeyIDObj(keyID){
-	return {
-		id:keyID
-	}
+    return {
+        id:keyID
+    }
 }
