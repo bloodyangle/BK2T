24604ae05ca61a270ffb6d20ff2d9d759338e821
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2019-07-04 16:37:21 +0800

    no message

--- handlers/batchmanager/batch_manager.py
+++ handlers/batchmanager/batch_manager.py
@@ -7,7 +7,7 @@ from sqlalchemy import desc
 from dbset.database.db_operate import db_session,pool
 from dbset.main.BSFramwork import AlchemyEncoder
 from models.SystemManagement.system import BatchInfoDetail, EletronicBatchDataStore, Equipment, BatchType, \
-    ElectronicBatchTwo, BatchInfo, AuditTrace
+    ElectronicBatchTwo, BatchInfo, AuditTrace, BrandFlag
 from dbset.log.BK2TLogger import logger,insertSyslog
 from flask_login import login_required, logout_user, login_user,current_user,LoginManager
 from tools.common import insert,delete,update
@@ -259,8 +259,8 @@ def DataHistorySelect():
                             r = i[2]
                             if not r:
                                 r = ""
-                            dir.append([t, r])
-                            dic.append([t, v])
+                            dir.append([t, v])
+                            dic.append([t, r])
                         div["WD"] = dic
                         div["YL"] = dir
                     elif Name == "浓缩":
@@ -431,3 +431,49 @@ def BatchUpdate():
             insertSyslog("error", "电子批记录修改报错Error：" + str(e), current_user.Name)
             return json.dumps("电子批记录修改", cls=AlchemyEncoder,ensure_ascii=False)
 
+@batch.route('/BrandFlagPage')
+def BrandFlagPage():
+    return render_template('./ProductionManagement/BrandFlagPage.html')
+
+@batch.route('/BrandFlagSearch', methods=['POST', 'GET'])
+def BrandFlagSearch():
+    if request.method == 'GET':
+        data = request.values
+        try:
+            jsonstr = json.dumps(data.to_dict())
+            if len(jsonstr) > 10:
+                pages = int(data.get("offset"))
+                rowsnumber = int(data.get("limit"))
+                inipage = pages * rowsnumber + 0
+                endpage = pages * rowsnumber + rowsnumber
+                BrandName = data.get('BrandName')
+                if BrandName == None or BrandName == "":
+                    total = db_session.query(BrandFlag).order_by(("ID")).count()
+                    oclass = db_session.query(BrandFlag).order_by(("ID")).all()[inipage:endpage]
+                else:
+                    total = db_session.query(BrandFlag).filter(BrandFlag.BrandName.like(BrandName)).order_by(("ID")).count()
+                    oclass = db_session.query(BrandFlag).filter(BrandFlag.BrandName.like(BrandName)).order_by(("ID")).all()[inipage:endpage]
+                jsonoclass = json.dumps(oclass, cls=AlchemyEncoder, ensure_ascii=False)
+                return '{"total"' + ":" + str(total) + ',"rows"' + ":\n" + jsonoclass + "}"
+        except Exception as e:
+            print(e)
+            logger.error(e)
+            insertSyslog("error", "品名维护表Error：" + str(e), current_user.Name)
+
+@batch.route('/BrandFlagCreate', methods=['POST', 'GET'])
+def BrandFlagCreate():
+    if request.method == 'POST':
+        data = request.values  # 返回请求中的参数和form
+        return insert(BrandFlag, data)
+
+@batch.route('/BrandFlagUpdate', methods=['POST', 'GET'])
+def BrandFlagUpdate():
+    if request.method == 'POST':
+        data = request.values  # 返回请求中的参数和form
+        return update(BrandFlag, data)
+
+@batch.route('/BrandFlagDelete', methods=['POST', 'GET'])
+def BrandFlagDelete():
+    if request.method == 'POST':
+        data = request.values  # 返回请求中的参数和form
+        return delete(BrandFlag, data)
